2bd3ee38debb3478155e3ad6dccf29fef4e96c34
shemaarafati2020 <shemaarafati940@gmail.com>
2026-03-16 16:46:18 +0200

    chore: add Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/photo-1507003211169-0a1dd7228f2d/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.rollingstone.com',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'camo.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+}
+
+module.exports = nextConfig
